09c7fba4bfede29b5e3d2a6d964d784a12eef1cd
Alibek <ali.mazhikenov@gmail.com>
2021-09-30 01:07:17 +0600

    website first commit

---
+++ style/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("body").style.marginRight = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("body").style.marginRight = "0";
+  }
